9e876ebc1a1049728ff2c0d484647f7c82c03566
Karsten Silkenbäumer <ksi@kluks.de>
2015-12-19 00:16:57 +0100

    Add config file support

--- lib/hue_control.rb
+++ lib/hue_control.rb
@@ -1,6 +1,19 @@
 require "hue_control/version"
 require "hue_control/cli/application"
+require "yaml"
+
+CONFIG_FILE = "#{ENV['HOME']}/.huerc"
 
 module HueControl
-  # Your code goes here...
+  if File.exists?(CONFIG_FILE)
+    @@config = YAML.load(File.read(CONFIG_FILE))
+  else
+    unless ARGV.include?("init")
+      abort "Config file does not exist. Please create one first with `hue_control init`"
+    end
+  end
+
+  def self.config
+    @@config
+  end
 end
--- lib/hue_control/cli/application.rb
+++ lib/hue_control/cli/application.rb
@@ -4,6 +4,16 @@ module HueControl
   module Cli
     class Application < Thor
       include Thor::Actions
+
+      desc "init", "Create initialization file"
+      def init
+        if File.exists?(CONFIG_FILE)
+          abort unless yes?("Config file already exists. Sure you want to overwrite it?")
+        end
+        File.write(CONFIG_FILE, "---\nip: YOUR_IP_HERE\nusername: YOUR_USERNAME_HERE\n")
+        puts "Config file created."
+        puts "Please edit #{CONFIG_FILE} and enter IP of the hue base and your api username."
+      end
     end
   end
 end
